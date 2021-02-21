0985ad4b6c0175bf897b2a2f66a8777581b15c63
FabriJuncal <juncalfabri@gmail.com>
2021-02-21 20:29:07 -0300

    81. Mostrando los gastos en la interfaz

---
+++ src/components/Gasto.jsx
@@ -0,0 +1,10 @@
+const Gastos = ({gasto}) => (
+    <li className="gastos">
+        <p>
+            {gasto.nombre}
+            <span className="gasto">{gasto.cantidad}</span>
+        </p>
+    </li>
+);
+ 
+export default Gastos;
